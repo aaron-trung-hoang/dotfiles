7fd6aadabdc909d4d8f856f669b8b747dcfd15a8
Aaron Hoang <hoangquoctrung02@hotmail.com>
2026-08-16 10:57:33 +0700

    feat(nvim): plugins - catppuccin + git + statusline + which-key

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      term_colors = true,
+      transparent_background = false,
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
+}
