80efd94f3d2373e959496cf809ff6e083825b694
Anthony Hoang Quoc Trung <hoangquoctrung02@hotmail.com>
2026-08-16 16:29:13 +0700

    feat(nvim): some plugins

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,7 +1,13 @@
 return {
   {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {},
+    'NeogitOrg/neogit',
+    dependencies = { 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' },
+    keys = { { '<leader>g', function() require('neogit').open() end, desc = 'Neogit' } },
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    event = 'BufWinEnter',
+    opts = { current_line_blame = true },  -- who last touched this line
   },
 }
+
